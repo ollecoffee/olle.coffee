2d1e40a01b2d916e6672309e12425f90de2737a8
Olle Gynther Zillén <olle.gyntherzillen@avanza.se>
2023-10-06 09:45:33 +0200

    updates to astro

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,5 +1,4 @@
 import { z, defineCollection } from "astro:content";
-
 const blogSchema = z.object({
     title: z.string(),
     description: z.string(),
@@ -7,6 +6,9 @@ const blogSchema = z.object({
     updatedDate: z.string().optional(),
     heroImage: z.string().optional(),
     badge: z.string().optional(),
+    tags: z.array(z.string()).refine(items => new Set(items).size === items.length, {
+        message: 'tags must be unique',
+    }).optional(),
 });
 
 export type BlogSchema = z.infer<typeof blogSchema>;
