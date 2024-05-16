53472e19d0645f8ed505403bdddb0236b62955b9
Olle Gynther Zillén <olle.gyntherzillen@avanza.se>
2024-05-16 16:12:49 +0200

    major optimising

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,21 +1,21 @@
 import { z, defineCollection } from "astro:content";
-const blogSchema = z.object({
-    title: z.string(),
-    description: z.string(),
-    pubDate: z.coerce.date(),
-    updatedDate: z.string().optional(),
-    heroImage: z.string().optional(),
-    badge: z.string().optional(),
-    tags: z.array(z.string()).refine(items => new Set(items).size === items.length, {
-        message: 'tags must be unique',
-    }).optional(),
-    video: z.string().optional(),
+
+const blog = defineCollection({
+    schema: ({image}) =>
+        z.object({
+            title: z.string(),
+            description: z.string(),
+            pubDate: z.coerce.date(),
+            updatedDate: z.string().optional(),
+            heroImage: image().optional(),
+            badge: z.string().optional(),
+            tags: z.array(z.string()).refine(items => new Set(items).size === items.length, {
+                message: 'tags must be unique',
+            }).optional(),
+            video: z.string().optional(),
+        })
 });
 
-export type BlogSchema = z.infer<typeof blogSchema>;
-
-const blogCollection = defineCollection({ schema: blogSchema });
-
 export const collections = {
-    'blog': blogCollection,
+    'blog': blog,
 }
