7b1c5065b2b8b064f695c38bb979bed7d9660d15
Olle Korkmaz Zillén <olle.korkmazzillen@avanza.se>
2024-11-01 11:08:34 +0100

    new post

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,16 @@
-import { defineConfig } from 'astro/config';
+import {defineConfig} from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
-import { site } from './src/data/config.json';
+import {site} from './src/data/config.json';
 
 // https://astro.build/config
 export default defineConfig({
-  site: site.baseurl,
-  integrations: [mdx(), sitemap(), tailwind()]
+    site: site.baseurl,
+    integrations: [mdx(), sitemap(), tailwind()],
+    legacy: {
+        collections: true,
+    },
 });
