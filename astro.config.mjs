406813a7af513f67691083b371840ed7eef85115
Olle Korkmaz Zillén <olle.korkmazzillen@avanza.se>
2025-01-28 14:08:23 +0100

    :zap: tailwind with vite

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,9 +8,9 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+    site: site.baseurl,
+    integrations: [mdx(), sitemap()],
     vite: {
         plugins: [tailwindcss()],
     },
-    site: site.baseurl,
-    integrations: [mdx(), sitemap()],
 });
